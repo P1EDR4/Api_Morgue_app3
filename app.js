bc0cccbbd7010fa270a5c5ddadeb51a8d9e6f551
JesusALbert0 <paredesdzulja29@gmail.com>
2024-05-09 00:41:00 -0600

    Final

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://morgueapp.onrender.com'
   }));
 
 
